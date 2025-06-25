3a1178c34f3181acd5669e5921f96ddf88b9af89
DeepakHaraleQA94 <haraledeepak6@gmail.com>
2025-06-25 08:44:56 +0530

    new repo

---
+++ src/main/java/TestClasses/AboutUsPageValidation.java
@@ -0,0 +1,35 @@
+package TestClasses;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class AboutUsPageValidation {
+
+	
+	@Test
+	public void verifyHomePage() throws InterruptedException {
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.puneripattern.com/");
+		
+		driver.findElement(By.xpath("(//button[@type='button'])[7]")).click();
+		driver.findElement(By.linkText("About Us")).click();
+		
+		Thread.sleep(3000);
+		String getAboutPageTitle = driver.getTitle();
+		System.out.println(getAboutPageTitle);
+		
+		if(getAboutPageTitle.equals("About Us | Puneri Pattern")) {
+			System.out.println("TC Passed");
+		}else
+		{
+			System.out.println("TC failed");
+		}
+		
+		driver.close();
+		
+		
+	}
+}
---
+++ src/main/java/TestClasses/HomePageValidation.java
@@ -0,0 +1,29 @@
+package TestClasses;
+
+import org.jspecify.annotations.Nullable;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class HomePageValidation {
+
+	@Test
+	public void verifyHomePage() {
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.puneripattern.com/");
+		String getHomePageTitle = driver.getTitle();
+		System.out.println(getHomePageTitle);
+		
+		if(getHomePageTitle.equals("Software Testing | Full Stack Development Training Hadapsar")) {
+			System.out.println("TC Passed");
+		}else
+		{
+			System.out.println("TC failed");
+		}
+		
+		driver.close();
+		
+		
+	}
+}
